72ccb4286c678129ff0a50ed81e09d0e68d3da07
leonardo <leonardo.victorufpa@gmail.com>
2019-09-27 23:33:30 -0300

    Using API from github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import api from '../../services/api';
+
 import { Container, Form, SubmitButton } from './styles';
 
-function Main() {
-    return (
-        <Container>
-            <h1>
-                <FaGithubAlt />
-                Repositórios
-            </h1>
-            <Form onSubmit={() => {}}>
-                <input type="text" placeholder="Adicionar repositório" />
+export default class Main extends Component {
+    state = {
+        newRepo: '',
+    };
 
-                <SubmitButton>
-                    <FaPlus color="#FFF" size={14} />
-                </SubmitButton>
-            </Form>
-        </Container>
-    );
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        const { newRepo } = this.state;
+        const response = await api.get(`/repos/${newRepo}`);
+
+        console.log(response.data);
+    };
+
+    render() {
+        const { newRepo } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repositórios
+                </h1>
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositório"
+                        value={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <SubmitButton>
+                        <FaPlus color="#FFF" size={14} />
+                    </SubmitButton>
+                </Form>
+            </Container>
+        );
+    }
 }
-
-export default Main;
