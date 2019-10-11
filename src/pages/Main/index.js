b11fc7e2b27d7132d4a4751ae00ae6d70fde19d9
Leonardo <leonardo.victorufpa@gmail.com>
2019-10-11 09:40:45 -0300

    Add Repositories

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
 import api from '../../services/api';
 
@@ -8,6 +8,8 @@ import { Container, Form, SubmitButton } from './styles';
 export default class Main extends Component {
     state = {
         newRepo: '',
+        repositories: [],
+        loading: false,
     };
 
     handleInputChange = e => {
@@ -17,14 +19,24 @@ export default class Main extends Component {
     handleSubmit = async e => {
         e.preventDefault();
 
-        const { newRepo } = this.state;
+        this.setState({ loading: true });
+
+        const { newRepo, repositories } = this.state;
         const response = await api.get(`/repos/${newRepo}`);
 
-        console.log(response.data);
+        const data = {
+            name: response.data.full_name,
+        };
+
+        this.setState({
+            repositories: [...repositories, data],
+            newRepo: '',
+            loading: false,
+        });
     };
 
     render() {
-        const { newRepo } = this.state;
+        const { newRepo, loading } = this.state;
 
         return (
             <Container>
@@ -40,8 +52,12 @@ export default class Main extends Component {
                         onChange={this.handleInputChange}
                     />
 
-                    <SubmitButton>
-                        <FaPlus color="#FFF" size={14} />
+                    <SubmitButton loading={loading}>
+                        {loading ? (
+                            <FaSpinner color="#FFF" size={14} />
+                        ) : (
+                            <FaPlus color="#FFF" size={14} />
+                        )}
                     </SubmitButton>
                 </Form>
             </Container>
