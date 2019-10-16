85bc6e7bee14b6a6423fd83d49f3b4da37a716cc
Leonardo <leonardo.victorufpa@gmail.com>
2019-10-16 16:10:28 -0300

    Changing input border according if identify error

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -11,6 +11,7 @@ export default class Main extends Component {
         newRepo: '',
         repositories: [],
         loading: false,
+        error: null,
     };
 
     //Carregar os dados do localStorage
@@ -31,30 +32,46 @@ export default class Main extends Component {
     }
 
     handleInputChange = e => {
-        this.setState({ newRepo: e.target.value });
+        this.setState({ newRepo: e.target.value, error: false });
     };
 
     handleSubmit = async e => {
         e.preventDefault();
 
-        this.setState({ loading: true });
+        this.setState({ loading: true, error: '' });
 
-        const { newRepo, repositories } = this.state;
-        const response = await api.get(`/repos/${newRepo}`);
+        try {
+            const { newRepo, repositories } = this.state;
 
-        const data = {
-            name: response.data.full_name,
-        };
+            if (newRepo === '') {
+                throw new Error('Você precisa indicar um repositório');
+            }
 
-        this.setState({
-            repositories: [...repositories, data],
-            newRepo: '',
-            loading: false,
-        });
+            const hasRepo = repositories.find(r => r.name === newRepo);
+
+            if (hasRepo) {
+                throw new Error('Repositório duplicado');
+            }
+
+            const response = await api.get(`/repos/${newRepo}`);
+
+            const data = {
+                name: response.data.full_name,
+            };
+
+            this.setState({
+                repositories: [...repositories, data],
+                newRepo: '',
+            });
+        } catch (error) {
+            this.setState({ error: true });
+        } finally {
+            this.setState({ loading: false });
+        }
     };
 
     render() {
-        const { newRepo, loading, repositories } = this.state;
+        const { newRepo, loading, repositories, error } = this.state;
 
         return (
             <Container>
@@ -62,7 +79,8 @@ export default class Main extends Component {
                     <FaGithubAlt />
                     Repositórios
                 </h1>
-                <Form onSubmit={this.handleSubmit}>
+
+                <Form onSubmit={this.handleSubmit} error={error}>
                     <input
                         type="text"
                         placeholder="Adicionar repositório"
